c06a78983231fbeb6ad1a1af315c1908a11b1486
Ivan Borshukov <ivan.borshukov@sap.com>
2015-01-10 12:06:30 +0200

    Switch to new facade API

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/Bo0mer/os-agent/facade"
+	"github.com/Bo0mer/os-agent/jobstore"
 	"github.com/Bo0mer/os-agent/server"
 
 	"github.com/Bo0mer/executor"
@@ -13,17 +15,27 @@ import (
 
 func main() {
 
-	osAgentFacade := facade.NewOSAgentFacade(executor.NewExecutor())
+	osAgentFacade := facade.NewOSAgentFacade(executor.NewExecutor(), jobstore.NewJobStore())
 
-	commandHandler := server.NewHandler("POST", "/command", osAgentFacade.ExecuteCommand)
+	createJobHandler := server.NewHandler("POST", "/jobs", osAgentFacade.CreateJob)
+	getJobHandler := server.NewHandler("GET", "/jobs", osAgentFacade.GetJob)
 
 	s := server.NewServer("127.0.0.1", 8080)
-	s.Register(commandHandler)
+	s.Register(createJobHandler)
+	s.Register(getJobHandler)
 
-	s.Start()
-
+	fmt.Println("Starting...")
+	err := s.Start()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Start successful.")
 	osChan := make(chan os.Signal)
 	signal.Notify(osChan, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGINT)
 
 	<-osChan
+	fmt.Println("Shtutting down...")
+	s.Stop()
+	fmt.Println("Shutdown successsful.")
 }
