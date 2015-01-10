4211d8f60891474baac51d85ca71f7399cfdf579
Ivan Borshukov <ivan.borshukov@sap.com>
2015-01-10 12:04:14 +0200

    Redesign API

--- facade/OSAgentFacade_test.go
+++ facade/OSAgentFacade_test.go
@@ -2,15 +2,15 @@ package facade_test
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 
 	"github.com/Bo0mer/executor/fakes"
+	execModel "github.com/Bo0mer/executor/model"
 
 	. "github.com/Bo0mer/os-agent/facade"
+	. "github.com/Bo0mer/os-agent/jobstore/fakes"
 	. "github.com/Bo0mer/os-agent/model"
 	. "github.com/Bo0mer/os-agent/server/fakes"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -21,87 +21,227 @@ var _ = Describe("OSAgentFacade", func() {
 	var executor *fakes.FakeExecutor
 	var req *FakeRequest
 	var resp *FakeResponse
+	var fakeJobStore *FakeJobStore
+
+	var itBehavesLikeBadRequest = func() {
+		It("should return status Bad Request", func() {
+			Expect(resp.SetStatusCodeCallCount()).To(Equal(1))
+			Expect(resp.SetStatusCodeArgsForCall(0)).To(Equal(http.StatusBadRequest))
+		})
+	}
+
+	var doInvalidRequest = func() {
+		req.BodyReturns([]byte("invalid request"))
+	}
+
+	var doValidRequest = func(async bool) {
+		commandRequest := CommandRequest{
+			Name: "ls",
+			Args: []string{"-la"},
+		}
+
+		jobRequest := JobRequest{
+			Async:   async,
+			Command: commandRequest,
+		}
+
+		requestBody, _ := json.Marshal(jobRequest)
+		req.BodyReturns(requestBody)
+	}
 
 	BeforeEach(func() {
 		req = new(FakeRequest)
 		resp = new(FakeResponse)
 		executor = new(fakes.FakeExecutor)
-		osAgentFacade = NewOSAgentFacade(executor)
+		fakeJobStore = new(FakeJobStore)
+		osAgentFacade = NewOSAgentFacade(executor, fakeJobStore)
 	})
 
-	JustBeforeEach(func() {
-		osAgentFacade.ExecuteCommand(req, resp)
-	})
+	Describe("CreateJob", func() {
 
-	Context("When the request is invalid", func() {
-		BeforeEach(func() {
-			req.BodyReturns([]byte("invalid request"))
+		JustBeforeEach(func() {
+			osAgentFacade.CreateJob(req, resp)
 		})
 
-		It("should return an status bad request", func() {
-			Expect(resp.SetStatusCodeCallCount()).To(Equal(1))
-			Expect(resp.SetStatusCodeArgsForCall(0)).To(Equal(http.StatusBadRequest))
-		})
-
-		It("should have not executed a command", func() {
-			Expect(executor.ExecuteCallCount()).To(Equal(0))
-		})
-
-	})
-
-	Context("when the request is valid", func() {
-		var commandRequest CommandRequest
-
-		BeforeEach(func() {
-			commandRequest = CommandRequest{
-				Name: "ls",
-				Args: []string{"-la"},
-			}
-			requestBody, _ := json.Marshal(commandRequest)
-			req.BodyReturns(requestBody)
-
-		})
-
-		It("should have called the executor with the proper command", func() {
-			Expect(executor.ExecuteCallCount()).To(Equal(1))
-
-			execCmd := executor.ExecuteArgsForCall(0)
-			Expect(execCmd.Name).To(Equal("ls"))
-			Expect(execCmd.Args).To(Equal([]string{"-la"}))
-		})
-
-		Context("when running the command succeeds", func() {
+		Context("When the request is invalid", func() {
 			BeforeEach(func() {
-				executor.ExecuteReturns("stdout", "stderr", 0, nil)
+				doInvalidRequest()
 			})
 
-			It("should return status code 200 OK", func() {
-				Expect(resp.SetStatusCodeArgsForCall(0)).To(Equal(http.StatusOK))
+			itBehavesLikeBadRequest()
+
+			It("should have not executed a command", func() {
+				Expect(executor.ExecuteAsyncCallCount()).To(Equal(0))
 			})
 
-			It("should return the proper result", func() {
-				commandResponse := &CommandResponse{}
-				json.Unmarshal(resp.SetBodyArgsForCall(0), commandResponse)
+		})
 
-				Expect(commandResponse.Stdout).To(Equal("stdout"))
-				Expect(commandResponse.Stderr).To(Equal("stderr"))
-				Expect(commandResponse.ExitCode).To(Equal(0))
-				Expect(commandResponse.Error).To(Equal(""))
+		Context("when the request is valid", func() {
+			var actualJob Job
+
+			BeforeEach(func() {
+				executor.ExecuteAsyncStub = func(c execModel.Command) <-chan execModel.CommandResult {
+					resultChan := make(chan execModel.CommandResult, 1)
+					resultChan <- execModel.CommandResult{
+						Stdout:   "stdout",
+						Stderr:   "stderr",
+						ExitCode: 0,
+						Error:    nil,
+					}
+					return resultChan
+				}
+
+				fakeJobStore.SetStub = func(job Job) {
+					actualJob = job
+				}
+
+				fakeJobStore.GetStub = func(id string) (Job, bool) {
+					return actualJob, true
+				}
+
+			})
+
+			Context("and NOT async", func() {
+				BeforeEach(func() {
+					doValidRequest(false)
+				})
+
+				It("should have called the executor with the proper command", func() {
+					Expect(executor.ExecuteAsyncCallCount()).To(Equal(1))
+
+					execCmd := executor.ExecuteAsyncArgsForCall(0)
+					Expect(execCmd.Name).To(Equal("ls"))
+					Expect(execCmd.Args).To(Equal([]string{"-la"}))
+				})
+
+				It("should return status code 200 OK", func() {
+					Expect(resp.SetStatusCodeArgsForCall(0)).To(Equal(http.StatusOK))
+				})
+
+				It("should return the proper result", func() {
+					job := &Job{}
+					json.Unmarshal(resp.SetBodyArgsForCall(0), job)
+
+					Expect(*job).To(Equal(actualJob))
+				})
+
+			})
+
+			Context("and async", func() {
+				BeforeEach(func() {
+					doValidRequest(true)
+				})
+
+				It("should have called the executor with the proper command", func() {
+					Expect(executor.ExecuteAsyncCallCount()).To(Equal(1))
+
+					execCmd := executor.ExecuteAsyncArgsForCall(0)
+					Expect(execCmd.Name).To(Equal("ls"))
+					Expect(execCmd.Args).To(Equal([]string{"-la"}))
+				})
+
+				It("should have returned the proper job", func() {
+					returnedJob := &Job{}
+					json.Unmarshal(resp.SetBodyArgsForCall(0), returnedJob)
+
+					Expect(*returnedJob).To(Equal(actualJob))
+				})
+
+				It("should return status 200 OK", func() {
+					Expect(resp.SetStatusCodeArgsForCall(0)).To(Equal(http.StatusOK))
+				})
+			})
+
+		})
+
+	})
+
+	Describe("GetJob", func() {
+
+		JustBeforeEach(func() {
+			osAgentFacade.GetJob(req, resp)
+		})
+
+		Context("when the id parameter is missing", func() {
+			BeforeEach(func() {
+				req.ParamValuesReturns([]string{}, false)
+			})
+
+			It("should return status Bad Request", func() {
+				status := resp.SetStatusCodeArgsForCall(0)
+				Expect(status).To(Equal(http.StatusBadRequest))
+			})
+
+		})
+
+		Context("when there are more than one id params", func() {
+			BeforeEach(func() {
+				req.ParamValuesReturns([]string{"one", "two"}, true)
+			})
+
+			It("should return status Bad Request", func() {
+				status := resp.SetStatusCodeArgsForCall(0)
+				Expect(status).To(Equal(http.StatusBadRequest))
 			})
 		})
 
-		Context("when running the command fails", func() {
+		Context("when the request is valid", func() {
+
+			var jobId string
+
 			BeforeEach(func() {
-				executor.ExecuteReturns("", "", -1, errors.New("error"))
+				jobId = "id-of-the-job"
+				req.ParamValuesReturns([]string{jobId}, true)
 			})
 
-			It("should return the proper error", func() {
-				commandResponse := &CommandResponse{}
-				json.Unmarshal(resp.SetBodyArgsForCall(0), commandResponse)
+			Context("and the job is missing", func() {
+				BeforeEach(func() {
+					fakeJobStore.GetStub = func(id string) (Job, bool) {
+						return Job{}, false
+					}
 
-				Expect(commandResponse.Error).To(Equal("error"))
+				})
+
+				It("should have looked for the right job", func() {
+					searchedJobId := fakeJobStore.GetArgsForCall(0)
+					Expect(searchedJobId).To(Equal(jobId))
+				})
+
+				It("should return status code 400 Not Found", func() {
+					status := resp.SetStatusCodeArgsForCall(0)
+					Expect(status).To(Equal(http.StatusNotFound))
+				})
+
+			})
+
+			Context("and the job is present", func() {
+
+				var job Job
+
+				BeforeEach(func() {
+					job = Job{
+						Id: "id",
+					}
+					fakeJobStore.GetStub = func(id string) (Job, bool) {
+						return job, true
+					}
+				})
+
+				It("should return the job", func() {
+					responseBody := resp.SetBodyArgsForCall(0)
+					returnedJob := &Job{}
+					json.Unmarshal(responseBody, returnedJob)
+
+					Expect(*returnedJob).To(Equal(job))
+				})
+
+				It("should return status code 200 OK", func() {
+					status := resp.SetStatusCodeArgsForCall(0)
+					Expect(status).To(Equal(http.StatusOK))
+				})
 			})
 		})
 
 	})
+
 })
--- facade/osagent.go
+++ facade/osagent.go
@@ -2,73 +2,138 @@ package facade
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/Bo0mer/executor"
 	execModel "github.com/Bo0mer/executor/model"
 
+	. "github.com/Bo0mer/os-agent/jobstore"
 	. "github.com/Bo0mer/os-agent/model"
 	"github.com/Bo0mer/os-agent/server"
 )
 
 type OSAgentFacade interface {
-	ExecuteCommand(server.Request, server.Response)
+	CreateJob(server.Request, server.Response)
+	GetJob(server.Request, server.Response)
 }
 
 type osAgentFacade struct {
 	executor executor.Executor
+	jobs     JobStore
 }
 
-func NewOSAgentFacade(e executor.Executor) OSAgentFacade {
+func NewOSAgentFacade(e executor.Executor, jobStore JobStore) OSAgentFacade {
 	return &osAgentFacade{
 		executor: e,
+		jobs:     jobStore,
 	}
 }
 
-func (f *osAgentFacade) ExecuteCommand(req server.Request, resp server.Response) {
-	execCommand, err := f.createExecutorCommand(req.Body())
+func (f *osAgentFacade) CreateJob(req server.Request, resp server.Response) {
+	jobRequest, err := f.createJobRequest(req.Body())
 	if err != nil {
 		resp.SetStatusCode(http.StatusBadRequest)
 		return
 	}
 
-	stdout, stderr, exitCode, err := f.executor.Execute(*execCommand)
-	var errorString string
-	if err != nil {
-		errorString = err.Error()
-	}
-	commandResp := &CommandResponse{
-		Stdout:   stdout,
-		Stderr:   stderr,
-		ExitCode: exitCode,
-		Error:    errorString,
-	}
+	execCommand := f.createExecutorCommand(jobRequest.Command)
 
-	responseBody, err := json.Marshal(commandResp)
-	if err != nil {
-		resp.SetStatusCode(http.StatusInternalServerError)
+	if jobRequest.Async {
+		f.executeCommandAsync(execCommand, resp)
+		return
+	} else {
+		f.executeCommand(execCommand, resp)
+	}
+}
+
+func (f *osAgentFacade) GetJob(req server.Request, resp server.Response) {
+	jobIdValues, found := req.ParamValues("id")
+	if !found || len(jobIdValues) > 1 {
+		resp.SetStatusCode(http.StatusBadRequest)
 		return
 	}
+
+	jobId := jobIdValues[0]
+	job, found := f.jobs.Get(jobId)
+	if !found {
+		resp.SetStatusCode(http.StatusNotFound)
+		return
+	}
+
+	f.setResponseBody(job, resp)
+}
+
+func (f *osAgentFacade) executeCommandAsync(cmd *execModel.Command, resp server.Response) {
+	job := f.createJob()
+	resultChan := f.executor.ExecuteAsync(*cmd)
+	go f.waitForCommandResult(resultChan, job)
+
+	f.setResponseBody(job, resp)
+}
+
+func (f *osAgentFacade) executeCommand(cmd *execModel.Command, resp server.Response) {
+	job := f.createJob()
+	resultChan := f.executor.ExecuteAsync(*cmd)
+	f.waitForCommandResult(resultChan, job)
+
+	job, _ = f.jobs.Get(job.Id)
+	f.setResponseBody(job, resp)
+}
+
+func (f *osAgentFacade) waitForCommandResult(resultChan <-chan execModel.CommandResult, job Job) {
+	result := <-resultChan
+
+	job.Status = JOB_COMPLETED
+	job.Result.Stdout, job.Result.Stderr = result.Stdout, result.Stderr
+	job.Result.ExitCode = result.ExitCode
+
+	var errorString string
+	if result.Error != nil {
+		errorString = result.Error.Error()
+	}
+
+	job.Result.Error = errorString
+
+	f.jobs.Set(job)
+}
+
+func (f *osAgentFacade) createJobRequest(data []byte) (*JobRequest, error) {
+	jobRequest := &JobRequest{}
+	err := json.Unmarshal(data, jobRequest)
+	return jobRequest, err
+}
+
+func (f *osAgentFacade) createJob() Job {
+	job := Job{
+		Id:     f.generateUniqueId(),
+		Status: JOB_IN_PROCESS,
+	}
+
+	f.jobs.Set(job)
+
+	return job
+}
+
+func (f *osAgentFacade) createExecutorCommand(c CommandRequest) *execModel.Command {
+	return &execModel.Command{
+		Name:           c.Name,
+		Args:           c.Args,
+		Env:            c.Env,
+		UseIsolatedEnv: c.UseIsolatedEnv,
+		WorkingDir:     c.WorkingDir,
+		Stdin:          strings.NewReader(c.Input),
+	}
+}
+
+func (f *osAgentFacade) setResponseBody(job Job, resp server.Response) {
+	responseBody, _ := json.Marshal(job)
 	resp.SetBody(responseBody)
 	resp.SetStatusCode(http.StatusOK)
 }
 
-func (f *osAgentFacade) createExecutorCommand(requestBody []byte) (*execModel.Command, error) {
-	var commandReq = &CommandRequest{}
-	err := json.Unmarshal(requestBody, commandReq)
-	if err != nil {
-		return nil, err
-	}
-
-	execCmd := &execModel.Command{
-		Name:           commandReq.Name,
-		Args:           commandReq.Args,
-		Env:            commandReq.Env,
-		UseIsolatedEnv: commandReq.UseIsolatedEnv,
-		WorkingDir:     commandReq.WorkingDir,
-		Stdin:          strings.NewReader(commandReq.Input),
-	}
-
-	return execCmd, nil
+func (f *osAgentFacade) generateUniqueId() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
