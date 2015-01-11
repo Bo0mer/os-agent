d642a964cc9b119be4212d04407c6b076c3212f3
Ivan Borshukov <ivan.borshukov@sap.com>
2015-01-11 16:34:47 +0200

    Adopt MasterClient

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/Bo0mer/os-agent/facade"
 	"github.com/Bo0mer/os-agent/jobstore"
+	. "github.com/Bo0mer/os-agent/masterclient"
 	"github.com/Bo0mer/os-agent/server"
 
 	l4g "code.google.com/p/log4go"
@@ -31,11 +33,40 @@ func main() {
 		return
 	}
 	l4g.Info("Start successful.")
+
+	c := NewMasterClient("http://127.0.0.1")
+	stop := make(chan struct{})
+	go sendHeartbeat(c, stop)
+
 	osChan := make(chan os.Signal)
 	signal.Notify(osChan, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGINT)
 
 	<-osChan
+	stop <- struct{}{}
+
 	l4g.Info("Shtutting down HTTP server...")
 	s.Stop()
 	l4g.Info("Shutdown successsful.")
+}
+
+func sendHeartbeat(c MasterClient, stop <-chan struct{}) {
+	for {
+		select {
+		case <-time.After(time.Minute * 5):
+			sendRegister(c)
+		case <-stop:
+			l4g.Debug("Stop heartbeat sending.")
+			return
+		}
+	}
+}
+
+func sendRegister(c MasterClient) {
+	l4g.Debug("Sending heartbeat to master...")
+	err := c.Register()
+	if err != nil {
+		l4g.Error("Error registering on master: %s", err.Error())
+		return
+	}
+	l4g.Info("Heartbeat successfuly sent.")
 }
